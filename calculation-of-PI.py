69c6e5756e8889af80d660a577932e8bb556f869
Raihan Kabir <iraihankabir@yahoo.com>
2017-08-26 00:00:57 +0600

    calculation of pi using polynomial in Thread way

---
+++ calculation-of-PI.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+	Working formula 
+			PI = 4.0 * ( 1 - 1/3 + 1/5 - 1/7 + 1/9 - ...)
+'''
+
+from threading import Thread as td
+import time
+
+maxLimit = 300000001
+negSUM   = 0.0
+posSUM   = 0.0
+
+def minusPart():
+	global negSUM
+	global maxLimit
+	start  = time.time()
+	for n in range(3, maxLimit, 4): 
+		negSUM += 1.0 / float(n)
+	res = "\n\tNegative part = " + str(negSUM) + " calculated in " + str(round(time.time() - start, 2)) + ' secs'
+	print(res)
+
+def plusPart():
+	global posSUM
+	global maxLimit
+	start = time.time()
+	for n in range(5, maxLimit, 4):
+		posSUM += 1.0 / float(n)
+
+	res = "\n\tPositive part = " + str(posSUM) + " calculated in " + str(round(time.time() - start, 2)) + " secs\n"
+	print(res)
+
+def parallelCal():
+	stTime = time.time()
+	p1     = td(target=minusPart, args=())
+	p2     = td(target=plusPart, args=())
+	p1.start()
+	p2.start()
+	p1.join()
+	p2.join()
+	PI = 4.0 * ( 1 - negSUM + posSUM )
+	print("\tPI            =", PI, "calculated in", round(time.time()-stTime, 2),'secs\n')
+
+def nonP():
+	st = time.time()
+	s1 = 0.0
+	s2 = 0.0
+	for i, j in zip(range(3, 300000001, 4), range(5, 300000001, 4)):
+		s1 += 1.0 / float(i)
+		s2 += 1.0 / float(j)
+
+	print((4.0 * (1-s1+s2)))
+	print((time.time()-st))
+
+
+if __name__ == "__main__":
+	parallelCal()
+	#nonP()
